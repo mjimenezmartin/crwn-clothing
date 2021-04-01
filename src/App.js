2a1cb5e7c64d155c6ab15612e53fa554aa28d756
Moises Jimenez <moises.jimenez@rindus.de>
2021-04-01 13:40:42 +0200

    Add naviagtion to menu-items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 
+function HatsPage({match: {params}}) {
+  return (
+    <div>
+      <h1>{params.param}</h1>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/:param' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
